2f5d25cbaef10ee19b6e04ba5392205bf7ea25a4
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2026-09-18 22:47:02 +0530

    Initial commit

---
+++ Tailwind/test/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#9333ea",
+      },
+    },
+  },
+};
